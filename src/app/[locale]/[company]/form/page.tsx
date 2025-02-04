30e9f512b9e44833cd3bf03812b87150740cf3d2
AmineGuitouni <Notam1ne2002@gmail.com>
2025-02-04 13:02:10 +0100

    start

---
+++ src/app/[locale]/[company]/form/page.tsx
@@ -0,0 +1,15 @@
+import { supabase } from "@/lib/supabase";
+export default function FormPage({ params: { company } }: { params: { company: string } }) {
+    async function action(data: FormData) {
+        "use server";
+        const name = data.get('name');
+        await supabase.from('data').insert({ name, company });
+    }
+
+    return (
+        <form action={action}>
+            <input name="name" type="text" />
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
---
+++ src/app/[locale]/[company]/workspace/page.tsx
@@ -0,0 +1,23 @@
+import { supabase } from "@/lib/supabase";
+import { createClient } from '@supabase/supabase-js'
+
+export default async function WorkspacePage({ params: { company } }: { params: { company: string } }) {
+    const {data, error} = await supabase.from("keys").select("keys").eq("company", company).single();
+    if(error) {
+        console.log(error)
+    }
+    console.log(data)
+    const localsb = createClient(data!.keys.NEXT_PUBLIC_SUPABASE_URL, data!.keys.SUPABASE_KEY);
+    const { data: localData, error: localError } = await localsb.from("data").select("*");
+
+    if (localError) {
+        console.log(localError);
+    }
+
+    console.log(localData);
+    return (
+        <div>
+            <h1>Workspace for {company} {localData ? localData[0].id : ""}</h1>
+        </div>
+    )
+}
